65075d065eac751b173aa91c67571d5882c2923d
alk3pInjection <webmaster@raspii.tech>
2025-10-21 06:16:25 +0800

    Re-import stripped Arm GNU Toolchain 14.2.Rel1
    lto fails on 14.3 and later
    
    Change-Id: I78ffc6f680d40cdc7cba2feb19e800d25ca54800

--- aarch64-none-linux-gnu/libc/usr/lib64/libstdc++.so.6.0.33-gdb.py
+++ aarch64-none-linux-gnu/libc/usr/lib64/libstdc++.so.6.0.33-gdb.py
@@ -19,7 +19,7 @@ import gdb
 import os
 import os.path
 
-pythondir = '/share/gcc-14.3.1/python'
+pythondir = '/share/gcc-14.2.1/python'
 libdir = '/aarch64-none-linux-gnu/lib/../lib64'
 
 # This file might be loaded when there is no current objfile.  This
